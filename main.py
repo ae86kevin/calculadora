27b3561c85bf75228a28004ccfef39865efa7ceb
ltkfa <ae86mm@gmail.com>
2025-09-04 23:14:11 -0600

    continuacion

--- main.py
+++ main.py
@@ -34,6 +34,38 @@ def funcuines(seleccion ):
     entrada3.delete(0, tk.END)
     entrada3.insert(0, resultado)
 
+def botonSuma():
+    a=float(entrada.get())
+    b=float(entrada.get())
+    resultado = suma(a, b)
+    entrada3.delete(0, tk.END)
+    entrada3.insert(0, resultado)
+
+def botonResta():
+    a=float(entrada.get())
+    b=float(entrada.get())
+    resultado = resta(a, b)
+    entrada3.delete(0, tk.END)
+    entrada3.insert(0, resultado)
+
+def botonMultiplicacion():
+    a=float(entrada.get())
+    b=float(entrada.get())
+    resultado = multiplicacion(a, b)
+    entrada3.delete(0, tk.END)
+    entrada3.insert(0, resultado)
+
+def botonDivision():
+    a=float(entrada.get())
+    b=float(entrada.get())
+    resultado = division(a, b)
+    entrada3.delete(0, tk.END)
+    entrada3.insert(0, resultado)
+
+
+
+
+
 
 ventana=tk.Tk()
 ventana.title("Calculadora")
@@ -53,7 +85,23 @@ etiqueta3= tk.Label(ventana, text="RESULTADO: ")
 etiqueta3.pack(padx= 7, pady=7)
 entrada3 = tk.Entry(ventana)
 entrada3.pack()
-print(entrada.get())
+
+
+bSuma= tk.Button(ventana, text="SUMA", command=botonSuma)
+bSuma.pack(padx=5, pady=5)
+
+bResta= tk.Button(ventana, text="RESTA", command=botonResta)
+bResta.pack(padx=5, pady=5)
+
+bMultiplicacion=tk.Button(ventana, text="MULTIPLICACION", command=botonMultiplicacion)
+bMultiplicacion.pack(padx=5, pady=5)
+
+bdivision= tk.Button(ventana, text="DIVISION", command=botonDivision)
+bdivision.pack(padx=5, pady=5)
+
+ventana.mainloop()
+
+
 
 
 
